d6f89d104b6ddea7b1da2c243994645d8e0a88a4
Rachik ABIDI <rachik.abidi@kisio.org>
2019-05-09 16:16:31 +0200

    🐛 Fix build with Swift 5.0

--- NavitiaSDK/Classes/Frameworks/AlamofireObjectMapper/AlamofireObjectMapper.swift
+++ NavitiaSDK/Classes/Frameworks/AlamofireObjectMapper/AlamofireObjectMapper.swift
@@ -101,11 +101,13 @@ extension DataRequest {
                 return .failure(error)
             }
             
-            let JSONObject = processResponse(request: request, response: response, data: data, keyPath: keyPath)
-            
-            if let JSONObject = JSONObject,
-                let parsedObject = (try? Mapper<T>(context: context, shouldIncludeNilValues: false).map(JSONObject: JSONObject)){
-                return .success(parsedObject)
+            if let currentJSONObject = processResponse(request: request, response: response, data: data, keyPath: keyPath) {
+                let mapper = Mapper<T>(context: context, shouldIncludeNilValues: false)
+                do {
+                    let parsedObject = try mapper.map(JSONObject: currentJSONObject)
+                    // if parsedObject != nil { // if let parsedObject = parsedObject {
+                    return .success(parsedObject)
+                } catch {}
             }
             
             let failureReason = "ObjectMapper failed to serialize response."
@@ -160,11 +162,13 @@ extension DataRequest {
                 return .failure(error)
             }
             
-            if let JSONObject = processResponse(request: request, response: response, data: data, keyPath: keyPath){
-                
-                if let parsedObject = try? Mapper<T>(context: context, shouldIncludeNilValues: false).mapArray(JSONObject: JSONObject){
+            if let currentJSONObject = processResponse(request: request, response: response, data: data, keyPath: keyPath) {
+                let mapper = Mapper<T>(context: context, shouldIncludeNilValues: false)
+                do {
+                    let parsedObject = try mapper.mapArray(JSONObject: currentJSONObject)
+                    // if parsedObject != nil { // if let parsedObject = parsedObject {
                     return .success(parsedObject)
-                }
+                } catch {}
             }
             
             let failureReason = "ObjectMapper failed to serialize response."
